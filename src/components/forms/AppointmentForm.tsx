16fc201ce01dd5fdc60e3995098e749cd28dc4ac
ManuMarcos <manuelignaciomarcos@hotmail.com>
2025-02-13 18:21:58 -0300

    Feat: Appointment section v0.1 added

---
+++ src/components/forms/AppointmentForm.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { Availability } from "./Availability";
+
+export const AppointmentForm = () => {
+  const [type, setType] = useState("");
+  const [selection, setSelection] = useState("");
+  const types = ["Servicio", "Paquete"];
+  const [itemsList, setItemsList] = useState([]);
+
+  const handleSelection = (type) => {
+    setType(type);
+    fetchData(type);
+  };
+
+  const token =
+    "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJBRE1JTiJdLCJ1c2VySWQiOjEsImp0aSI6IjcxM2E1MWZiLWEzZDUtNDAxYi1hZDdlLWY3MzhkMTI3ODViYSIsInN1YiI6ImRlZmF1bHRhZG1pbkBjbGluaWNhLmNvbSIsImlhdCI6MTczOTQ2ODgwNSwiZXhwIjoxNzM5NTU1MjA1fQ.vyZfMC-z5SN3H1RMIavV0F_3V6QabcS6Imau7nIrjfQ";
+
+  const fetchData = async (type) => {
+    try {
+      let url = `http://localhost:8080/${
+        type === "Paquete" ? "packages" : "services"
+      }`;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error("Error en la solicitud");
+
+      const data = await response.json();
+      const mappedData =
+        type === "Paquete"
+          ? data.map((packageItem) => ({
+              label: `${packageItem.package_code} (${packageItem.services
+                .map((service) => service.name)
+                .join(", ")})`,
+              value: packageItem.id,
+            }))
+          : data.map((service) => ({
+              label: service.name,
+              value: service.id,
+            }));
+      setItemsList(mappedData);
+    } catch (error) {
+      console.log("Error fetch data: ", error);
+    }
+  };
+
+  return (
+    <div className="p-6 max-w-lg mx-auto bg-white shadow-md rounded-lg">
+      <label className="text-xl font-bold mb-4">
+        Hola nombre_usuario, ¿Cómo te podemos ayudar?
+      </label>
+
+      <div className="form-group">
+        <label className="font-semibold mb-2">
+          ¿Para qué estás buscando un turno?
+        </label>
+        <div className="btn-align ">
+          {types.map((option) => (
+            <button
+              key={option}
+              className={`btn btn-ok p-2 rounded-md bg-orange-500 text-white`}
+              onClick={() => handleSelection(option)}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {type && (
+        <div className="step">
+          <div className="form-group select-a">
+            <label className="select-label">Selecciona un {type}</label>
+            <select
+              value={selection}
+              onChange={(e) => setSelection(e.target.value)}
+            >
+              <option value="" disabled>
+                Selecciona una opción
+              </option>
+              {itemsList.map(({ label, value }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </select>
+            <div className="mt-4 flex justify-between"></div>
+          </div>
+        </div>
+      )}
+
+      {type === "Servicio" && selection && (
+        <Availability serviceId={selection} token={token}></Availability>
+      )}
+    </div>
+  );
+};
---
+++ src/components/forms/Availability.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { DoctorCard } from "../cards/DoctorCard";
+
+export interface TimeSlot {
+  time: String;
+  duration_in_minutes: number;
+}
+
+export interface AvailableDate {
+  date: string;
+  times: TimeSlot[];
+}
+
+export interface DoctorAvailability {
+  doctor_id: number;
+  doctor_name: string;
+  doctor_speciality: string;
+  image: string;
+  available_slots: AvailableDate[];
+}
+
+export const Availability = ({ serviceId, token }) => {
+  const [availabilities, setAvailabilities] = useState<DoctorAvailability[]>(
+    []
+  );
+
+  const getFormattedDate = (daysToAdd = 0) => {
+    const date = new Date();
+    date.setDate(date.getDate() + daysToAdd);
+    return date.toISOString().split("T")[0];
+  };
+
+  useEffect(() => {
+    const fetchAvailability = async () => {
+      let url =
+        `http://localhost:8080/appointments/availability?serviceId=${serviceId}` +
+        `&from=${getFormattedDate()}` +
+        `&to=${getFormattedDate(5)}`;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      setAvailabilities(data);
+    };
+
+    fetchAvailability();
+  }, [serviceId]);
+
+  const handleClick = (date, time, doctorId) => {
+    console.log(doctorId)
+    console.log(date);
+    console.log(time)
+  }
+
+  return (
+    <div>
+        <h4>Turnos Disponibles:</h4>
+      {availabilities.length > 0 ? (
+        <div className="availabilities">
+          {availabilities.map((doctor) => (
+            <div>
+            <DoctorCard key={doctor.doctor_id} doctor={doctor} onSelectSlot={handleClick}></DoctorCard>
+            <hr></hr>
+            </div>
+          ))}
+        </div>
+      ) : 
+        <div>
+            <h4>No hay turnos disponibles..</h4>
+        </div>
+      }
+    </div>
+  );
+};
